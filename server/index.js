eb891bab0b3b2b3c14b2c7e4e1dcf5a92f47d511
Moshe Plotkin <plotkinm@newpaltz.edu>
2025-02-17 15:15:08 -0500

    starting nodeJs

---
+++ server/index.js
@@ -0,0 +1,21 @@
+/*  B"H
+*/
+// Load the http module to create an http server.
+const http = require('http')
+const PORT = 8000
+
+// Configure our HTTP server to respond with Hello World to all requests.
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello World\n');
+});
+
+// Listen on port 8000, IP defaults to
+//
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+
+
+console.log('Hello World!');
