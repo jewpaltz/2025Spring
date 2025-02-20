2dd823711f6f84f3b02ade366996118fa32f7ad9
Moshe Plotkin <plotkinm@newpaltz.edu>
2025-02-20 15:19:34 -0500

    upgraded to express

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
 /*  B"H
 */
 // Load the http module to create an http server.
-const http = require('http')
+const express = require('express')
 const PORT = 8000
 
-// Configure our HTTP server to respond with Hello World to all requests.
-const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello New Paltz, NY!!!\n');
-});
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello New Paltz, NY!!!')
+})
 
 // Listen on port 8000, IP defaults to
 //
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`)
 });
 
 
 
-console.log('Hello World!');
+console.log('Hello World!')
