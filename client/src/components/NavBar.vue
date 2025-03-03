7309440afc828c10c54881a9a16e0bf4aecfe0d7
Moshe Plotkin <plotkinm@newpaltz.edu>
2025-03-03 14:46:57 -0500

    made navbar dropdown work

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+const isActive = ref(false)
 
 </script>
 
@@ -11,7 +14,7 @@
                 </a>
 
                 <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
-                   data-target="navbarBasicExample">
+                   :class="{ 'is-active': isActive }" @click="isActive = !isActive">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
@@ -19,7 +22,7 @@
                 </a>
             </div>
 
-            <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-menu" :class="{ 'is-active': isActive }">
                 <div class="navbar-start">
 
 
