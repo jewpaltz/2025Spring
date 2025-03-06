2579d145e0b0a44cfca51b80946a7ab5639b2934
Moshe Plotkin <plotkinm@newpaltz.edu>
2025-03-06 15:51:11 -0500

    Shopping Cart

--- client/src/components/ShoppingCart.vue
+++ client/src/components/ShoppingCart.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
+import { refCart } from '@/models/cart';
 
+const cart = refCart(); 
 </script>
 
 <template>
-    <div>
+    <div class="cart">
         <h2 class="title is-4">
             Shopping Cart
         </h2>
+        <ul>
+            <li v-for="item in cart" :key="item.id">
+                {{ item.name }} - {{ item.price }}
+            </li>
+        </ul>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.cart {
+    margin: 1rem;
+}
+</style>
---
+++ client/src/models/products.ts
@@ -0,0 +1,10 @@
+const items = [
+  { name: 'Apple', price: 1.25 },
+  { name: 'Banana', price: 0.75 },
+  { name: 'Cherry', price: 2.25 },
+  { name: 'Date', price: 3.25 },
+]
+
+export function getProducts() {
+  return items
+}
