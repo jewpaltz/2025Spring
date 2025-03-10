beefb2c2d386ca6f65ac06d6941d5c0209434a47
Moshe Plotkin <plotkinm@newpaltz.edu>
2025-03-10 14:54:43 -0400

    listing products

--- client/src/models/products.ts
+++ client/src/models/products.ts
@@ -1,10 +1,63 @@
-const items = [
-  { name: 'Apple', price: 1.25 },
-  { name: 'Banana', price: 0.75 },
-  { name: 'Cherry', price: 2.25 },
-  { name: 'Date', price: 3.25 },
-]
+/* B"H
+ */
 
-export function getProducts() {
-  return items
+import products from '../data/products.json'
+import type { DataListEnvelope } from './dataEnvelopes'
+
+export interface ProductDimensions {
+  width: number
+  height: number
+  depth: number
 }
+
+export interface ProductReview {
+  rating: number
+  comment: string
+  date: string
+  reviewerName: string
+  reviewerEmail: string
+}
+
+export interface ProductMeta {
+  createdAt: string
+  updatedAt: string
+}
+
+export interface Product {
+  id: number
+  title: string
+  description: string
+  category: string
+  price: number
+  rating?: number
+  stock?: number
+  tags?: string[]
+  brand?: string
+  sku?: string
+  weight?: number
+  dimensions?: ProductDimensions
+  shippingInformation?: string
+  availabilityStatus?: string
+  reviews?: ProductReview[]
+  returnPolicy?: string
+  minimumOrderQuantity?: number
+  meta?: ProductMeta
+  images?: string[]
+  thumbnail?: string
+}
+
+export function getAll() {
+  return products as DataListEnvelope<Product>
+}
+
+export function getOne(id: string) {
+  return products.items.find((item) => item.id == +id) as Product
+}
+
+getAll().items.push({
+  id: 100,
+  title: 'Test Product',
+  description: 'This is a test product',
+  category: 'test',
+  price: 100,
+})
--- client/src/pages/products/index.vue
+++ client/src/pages/products/index.vue
@@ -1,11 +1,54 @@
 <script setup lang="ts">
+import { getAll, type Product } from '@/models/products';
 
+const products = getAll()
+
+function addToCart(product: Product) {
+    console.log('Add to cart', product)
+}
 </script>
 
 <template>
     <div>
         <h1 class="title">Products</h1>
+        <div class="shelf">
+            <div class="product" v-for="p in products.items" :key="p.id">
+                <div class="product-image">
+                    <img :src="p.thumbnail" alt="product image" />
+                </div>
+                <div class="product-info">
+                    <h2>{{ p.title }}</h2>
+                    <p>{{ p.description }}</p>
+                    <span class="price">${{ p.price }}</span>
+                    <button class="button is-success" @click="addToCart(p)">Add to cart</button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.shelf {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+.shelf .product {
+    width: 30%;
+    margin: 1em;
+    padding: 1em;
+    border: 1px solid #ccc;
+    border-radius: 1em;
+}
+
+.price {
+    color: crimson;
+    font-weight: bold;
+    font-size: 1.5em;
+}
+
+.button.is-success {
+    float: right;
+}
+</style>
