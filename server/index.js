6302a3b80e116d7d35d9746451c1d7adc0308457
Moshe Plotkin <plotkinm@newpaltz.edu>
2025-03-24 15:17:51 -0400

    Started setting up server api routing

--- server/index.js
+++ server/index.js
@@ -2,13 +2,17 @@
 */
 // Load the http module to create an http server.
 const express = require('express')
+const productsController = require('./controllers/products')
+
 const PORT = 8000
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello New Paltz, NY!!!')
-})
+app
+  .get('/', (req, res) => {
+    res.send('Hello New Paltz, NY!!!')
+  })
+  .use('/products', productsController)
 
 // Listen on port 8000, IP defaults to
 //
