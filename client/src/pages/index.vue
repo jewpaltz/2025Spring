3fa5e454643f9c86f5add754ddfb15747e3921a1
Moshe Plotkin <plotkinm@newpaltz.edu>
2025-03-24 12:06:59 -0400

    post for demo

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -5,6 +5,14 @@ var bestClass = 'Web Programming'
 
 <template>
   <main>
-    <h1 class="title">Welcome to {{ bestClass }}</h1>
+    <h1 class="title">Anonymous Feedback Form</h1>
+    <h2 class="subtitle">Enter your feedback here...</h2>
+    <form action="#" class="form" method="post">
+      <textarea
+                class="textarea"
+                placeholder="Tell me about your favorite class">
+      </textarea>
+      <button class="button is-success" type="submit">Submit</button>
+    </form>
   </main>
 </template>
