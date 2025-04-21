6e661c152e0856e10f9b61879e1c279215f4edf8
Moshe Plotkin <plotkinm@newpaltz.edu>
2025-04-21 15:17:23 -0400

    User Session

--- client/src/components/LoginBadge.vue
+++ client/src/components/LoginBadge.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 
+import { isLoggedIn, login, logout, refSession } from '@/models/session';
 import { get, getAll, type User } from '@/models/users';
 import { ref } from 'vue'
 const users = ref<User[]>([])
@@ -8,10 +9,12 @@ getAll().then((response) => {
     users.value = response.items
 })
 
+const session = refSession()
+
 </script>
 
 <template>
-    <div class="buttons">
+    <div class="buttons" v-if="!isLoggedIn()">
         <a class="button is-primary">
             <strong>Sign up</strong>
         </a>
@@ -22,11 +25,19 @@ getAll().then((response) => {
             </a>
 
             <div class="navbar-dropdown">
-                <a class="navbar-item" v-for="user in users" :key="user.id">
+                <a class="navbar-item" v-for="user in users" :key="user.id" @click="login(user.id)">
                     {{ user.firstName }} {{ user.lastName }}
                 </a>
             </div>
         </div>
+    </div>
+    <div class="buttons" v-else>
+        <a class="button is-light" @click="logout()">
+            Log out
+        </a>
+        Hello
+        {{ session?.user?.firstName }} {{ session?.user?.lastName }}
+
     </div>
 </template>
 
--- client/src/models/session.ts
+++ client/src/models/session.ts
@@ -1,7 +1,32 @@
 /*  B"H
  */
+import { ref } from 'vue'
 import * as myFetch from './myFetch'
+import { get, type User } from './users'
 
 export function api<T>(action: string): Promise<T> {
   return myFetch.api<T>(action)
+}
+
+const session = ref({
+  user: null as User | null,
+  token: null as string | null,
+})
+
+export function refSession() {
+  return session
+}
+
+export const isAdmin = () => session.value?.user?.role === 'admin'
+
+export const isLoggedIn = () => session.value?.user !== null
+
+export function login(id: string) {
+  return get(id).then((user) => {
+    session.value.user = user
+  })
+}
+export function logout() {
+  session.value.user = null
+  session.value.token = null
 }
