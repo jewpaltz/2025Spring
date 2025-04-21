cdb0a997c9d84e7c2b9265c332263b9121616def
Moshe Plotkin <plotkinm@newpaltz.edu>
2025-04-21 15:08:08 -0400

    getting users on client

---
+++ client/src/components/LoginBadge.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+import { get, getAll, type User } from '@/models/users';
+import { ref } from 'vue'
+const users = ref<User[]>([])
+
+getAll().then((response) => {
+    users.value = response.items
+})
+
+</script>
+
+<template>
+    <div class="buttons">
+        <a class="button is-primary">
+            <strong>Sign up</strong>
+        </a>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+            <a class="button is-light">
+                Log in
+            </a>
+
+            <div class="navbar-dropdown">
+                <a class="navbar-item" v-for="user in users" :key="user.id">
+                    {{ user.firstName }} {{ user.lastName }}
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
