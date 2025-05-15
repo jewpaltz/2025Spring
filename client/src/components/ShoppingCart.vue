a4753ddc960d60fd3df45c63e3497e79bddc655c
Moshe Plotkin <plotkinm@newpaltz.edu>
2025-05-15 13:39:56 -0400

    autocomplete client side

--- client/src/components/ShoppingCart.vue
+++ client/src/components/ShoppingCart.vue
@@ -1,14 +1,62 @@
 <script setup lang="ts">
 import { refCart } from '@/models/cart';
+import { ref } from 'vue';
 
-const cart = refCart(); 
+const cart = refCart();
+const newItemId = ref('');
+const showAddItem = ref(false);
+const options = ref([
+    { label: 'Apples', value: '1' },
+    { label: 'Bananas', value: '2' },
+    { label: 'Cherries', value: '3' },
+    { label: 'Dates', value: '4' },
+    { label: 'Eggplants', value: '5' },
+    { label: 'Figs', value: '6' },
+    { label: 'Grapes', value: '7' },
+    { label: 'Honeydew', value: '8' },
+    { label: 'Kiwi', value: '9' },
+    { label: 'Lemons', value: '10' }
+]);
+
 </script>
 
 <template>
     <div class="cart">
         <h2 class="title is-4">
+            <button style="float: right;" class="button is-primary" @click="showAddItem = !showAddItem">
+                <span class="icon ">
+                    <i class="fas fa-plus"></i>
+                </span>
+            </button>
             Shopping Cart ({{ cart.length }})
         </h2>
+        <div>
+
+            <div class="message is-link" v-show="showAddItem">
+                <div class="message-body">
+                    <form @submit.prevent="() => showAddItem = false">
+                        <div class="field">
+                            <div class="control">
+                                <o-autocomplete
+                                                v-model="newItemId"
+                                                :options="options"
+                                                option-value="id"
+                                                option-label="title"
+                                                option-thumbnail="thumbnail"
+                                                expanded
+                                                placeholder="Select a product"
+                                                icon="search"
+                                                clearable
+                                                open-on-focus>
+                                </o-autocomplete>
+                            </div>
+                        </div>
+                        <button type="submit" class="button is-primary">Add to Cart</button>
+
+                    </form>
+                </div>
+            </div>
+        </div>
         <ul>
             <li v-for="item in cart" :key="item.product.id">
                 <img :src="item.product.thumbnail" :alt="item.product.title" />
--- client/src/main.ts
+++ client/src/main.ts
@@ -4,11 +4,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Buefy from 'buefy'
+import Oruga from '@oruga-ui/oruga-next'
 
 const app = createApp(App)
 
-app.use(router).use(Buefy, {
-  defaultIconPack: 'fas',
-})
+app
+  .use(router)
+  .use(Buefy, {
+    defaultIconPack: 'fas',
+  })
+  .use(Oruga)
 
 app.mount('#app')
